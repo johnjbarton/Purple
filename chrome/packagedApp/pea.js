c1568fa0625c5be5c8abfc77fb7a8d366f10c551
johnjbarton <johnjbarton@johnjbarton.com>
2011-08-29 10:06:28 -0700

    Copy from orionhub to workaround merge failure

--- chrome/packagedApp/pea.js
+++ chrome/packagedApp/pea.js
@@ -9,12 +9,17 @@ var WebAppManager = (function () {
     purpleTargetID: 6565259,
   };
 
-  WebAppManager.initialize = function() {
-    // TODO read options from localStorage
+  WebAppManager.getHistoryOfURLs = function() {
+    var history = []; 
+    var nURLs = window.localStorage.length;
+    for (var i = 0; i < nURLs; i++) {
+      history.push(window.localStorage.key(i));
+    }
+    return history;
   };
 
-  WebAppManager.destroy = function() {
-    // TODO flush options to localStorage
+  WebAppManager.updateHistory = function(url) {
+    window.localStorage.setItem(url, "");
   };
 
   WebAppManager.loadTargetApp = function(url) {
@@ -22,27 +27,33 @@ var WebAppManager = (function () {
     var targetID = Math.floor(WebAppManager.purpleTargetID * (Math.random() + 1.0) );
     var createData = {
       url: url,
-      tabId: WebAppManager.purpleTargetID, 
+//      tabId: WebAppManager.purpleTargetID, 
       focused: false,
-      type: normal,
+      type: 'normal',
     };
     chrome.windows.create(createData, function onCreated(win) {
       console.log("created window for web app "+url, win);
+      WebAppManager.updateHistory(url);
     });
   };
-
+  return WebAppManager;
 }());
 
 function loadWebApp(event) {
   var urlInput = document.getElementById('webAppURL');
   var url = urlInput.value;
-  WebAppManager.loadWebApp(url);
+  WebAppManager.loadTargetApp(url);
 }
 
 function wireButton(event) {
   window.removeEventListener('load', wireButton, false);
   var button = document.getElementById('loadWebApp');
   button.addEventListener('click', loadWebApp, true);
+  var history = WebAppManager.getHistoryOfURLs();
+  if (history.length) {
+    var urlInput = document.getElementById('webAppURL');
+    urlInput.value = WebAppManager.history[history.length - 1];
+  }
 }
 
 window.addEventListener('load', wireButton, false);
