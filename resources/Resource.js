c91463002c582aa7e48fd3f0246db4aa601823c5
johnjbarton <johnjbarton@johnjbarton.com>
2011-10-18 15:51:34 -0700

    refactor to extract lib/Base.js

---
+++ resources/Resource.js
@@ -0,0 +1,14 @@
+// See Purple/license.txt for Google BSD license
+// Copyright 2011 Google, Inc. johnjbarton@johnjbarton.com
+
+define(['../lib/base.js', '../lib/domplate/lib/domplate','../resources/ResourceRep'], function (Base, domplate, ResourceRep) {
+  
+  var Resource = Base.extend({
+    initialize: function(url) {
+      this.url = url;
+      this.rep = ResourceRep;
+    }
+  });
+  
+  return Resource;
+});
