fba9ff55c5dc7a06d60e2e8361cde206d0d6e6be
johnjbarton <johnjbarton@johnjbarton.com>
2011-10-21 11:33:00 -0700

    Clean up tabs, spacing, order and logic

--- chrome/extension/pea.js
+++ chrome/extension/pea.js
@@ -10,27 +10,27 @@ var MonitorChrome = window.MonitorChrome;
 
  
 function debuggerError(error) {
-    if (error) {
-	  console.error(error.message, error);
-	} else {
-	  console.error("debuggerError with no chrome.extension.lastError");
-	}
-  };
+  if (error) {
+    console.error(error.message, error);
+  } else {
+    console.error("debuggerError with no chrome.extension.lastError");
+  }
+}
 
 function getOrigin(url) {
-    var splits = url.split('/');
-    var origin = splits.slice(0, 3).join('/');
-    return origin;
+  var splits = url.split('/');
+  var origin = splits.slice(0, 3).join('/');
+  return origin;
 }
 
 function startMonitor(url, tabId, errback) {
   var clientOrigin = getOrigin(url);
   return MonitorChrome.connect(clientOrigin, tabId, errback);
-};
+}
 
 function stopMonitor(errback) {
   MonitorChrome.disconnect(errback);
-};
+}
   
 function appendPurple(url, onLoadCallBack) {
   var iframe = document.createElement('iframe');
@@ -62,94 +62,40 @@ function insureNumber(n) {
 
 function promiseDebuggeeWindow() {
 
-    var createData = { url: 'about:blank', type: 'popup' };
+  var createData = { url: 'about:blank', type: 'popup' };
 
-   var width = parseInt(window.localStorage.getItem('windowWidth'));
-   createData.width = insureNumber(width) || (Math.floor(window.screen.availWidth / 2) - 2);
+  var width = parseInt(window.localStorage.getItem('windowWidth'));
+  createData.width = insureNumber(width) || (Math.floor(window.screen.availWidth / 2) - 2);
 
-   var height = parseInt(window.localStorage.getItem('windowHeight'));
-   createData.height = insureNumber(height) || window.screen.availHeight;
+  var height = parseInt(window.localStorage.getItem('windowHeight'));
+  createData.height = insureNumber(height) || window.screen.availHeight;
 
-   var left = parseInt(window.localStorage.getItem('windowLeft'));
-   createData.left = insureNumber(left) || window.screen.availLeft;
-   console.log("chrome.windows.create:", createData);
-   var deferred = Q.defer();
-   chrome.windows.create(createData, function(win) {
-     deferred.resolve(win);
-   });
-   return deferred.promise;
+  var left = parseInt(window.localStorage.getItem('windowLeft'));
+  createData.left = insureNumber(left) || window.screen.availLeft;
+  console.log("chrome.windows.create:", createData);
+  var deferred = Q.defer();
+  chrome.windows.create(createData, function(win) {
+    deferred.resolve(win);
+  });
+  return deferred.promise;
 };
 
 function getPurpleURL () {
-  purpleURL = localStorage['messageClientURL'];
-  if(!purpleURL)
+  var purpleURL = window.localStorage.getItem('messageClientURL');
+  if(!purpleURL) {
     purpleURL = "http://localhost:8080/file/f/purple.html";
+  }
   return purpleURL;
 }
 
-function onOuterWindowLoad(event) {
-  window.removeEventListener('load', onOuterWindowLoad, false);
- 
-  var win = promiseDebuggeeWindow(tabInfo);
-  var tabInfo = promiseTabInfo();  
-  var done = Q.join(tabInfo, win, function(tabInfo, win) {
-      var debuggeeTabInfo = win.tabs[0];
-      console.log("ready to load purple with tabInfo and win", tabInfo, win);
-      console.log("starting monitor debugee tab.id", debuggeeTabInfo.id);
-
-      var purpleURL = getPurpleURL();
-      // start the monitor and wait for purple to connect
-      var monitorReady = startMonitor(purpleURL, debuggeeTabInfo.id, debuggerError);
-      // load purple in the iframe and call back to monitor when set up
-      var purpleReady = promisePurpleReady(purpleURL);
-      var done = Q.join(monitorReady, purpleReady, function(monitorReady, purpleReady) {
-	      console.log("monitorReady: %o", monitorReady);
-              console.log("purpleReady %o", purpleReady);
-     	// we have a blank window, with debuggeeTabInfo.id being monitored. Load the page
-	      window.alert('ready to update');
-        chrome.tabs.update(debuggeeTabInfo.id, {url: tabInfo.url});
-        return (monitorReady && purpleReady) ? 'Purple ready' : 'FAIL';
-      });
-
-      var unloadOuterWindow = promiseUnloadOuterWindow();
-      Q.when(unloadOuterWindow, function () {
-        stopMonitor(debuggerError);
-        chrome.tab.remove(debugeeTabInfo.id);
-      });
-      return done;
-    });
-  Q.when(done, function(done) {
-    console.log("Status: "+done);
-      }, function() {
-          if (arguments[0] instanceof Error) {
-              var e = arguments[0];
-              e._stack = e.stack && e.stack.split('\n');
-	      console.error("Purple ERROR: "+e, e);
-	  } else {
-            console.error("Purple FAILED", arguments);
-          }
-      });  
-}
-
-chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-	console.log("onBeforeNavigate "+details.url+" in "+details.tabId+" at "+details.timeStamp);
-    });
-
-function promisePurpleReady() {
+function promisePurpleReady(purpleURL) {
   var deferred = Q.defer();
 
   appendPurple(purpleURL, function onPurpleLoad(){
-	  deferred.resolve(purpleURL);
+    deferred.resolve(purpleURL);
   });
   return deferred.promise;
 }
-
-function onWindowUnload(event) {
-  window.removeEventListener('unload', onWindowUnload, false);
-  stopMonitor(debuggerError);
-}
-
-window.addEventListener('load', onOuterWindowLoad, false);
 
 function promiseUnloadOuterWindow() {
     var deferred = Q.defer();
@@ -160,5 +106,73 @@ function promiseUnloadOuterWindow() {
     window.addEventListener('unload', handle, false);
     return deferred.promise;
 }
+
+function onOuterWindowLoad(event) {
+  window.removeEventListener('load', onOuterWindowLoad, false);
+ 
+  var win = promiseDebuggeeWindow();
+  var tabInfo = promiseTabInfo();  
+  var done = Q.join(tabInfo, win, function(tabInfo, win) {
+    var debuggeeTabInfo = win.tabs[0];
+  
+    console.log("ready to load purple with tabInfo and win", tabInfo, win);
+    console.log("starting monitor debugee tab.id", debuggeeTabInfo.id);
+
+    var purpleURL = getPurpleURL();
+    // start the monitor and wait for purple to connect
+    var monitorReady = startMonitor(purpleURL, debuggeeTabInfo.id, debuggerError);
+    // load purple in the iframe and call back to monitor when set up
+    var purpleReady = promisePurpleReady(purpleURL);
+  
+    var done = Q.join(monitorReady, purpleReady, function(monitorReady, purpleReady) {
+      console.log("monitorReady: %o", monitorReady);
+      console.log("purpleReady %o", purpleReady);
+      // we have a blank window, with debuggeeTabInfo.id being monitored. Load the page
+      window.alert('ready to update');
+      chrome.tabs.update(debuggeeTabInfo.id, {url: tabInfo.url});
+      return (monitorReady && purpleReady) ? 'Purple ready' : 'FAIL';
+    });
+
+    var unloadOuterWindow = promiseUnloadOuterWindow();
+    Q.when(unloadOuterWindow, function () {
+      stopMonitor(debuggerError);
+      chrome.tab.remove(debugeeTabInfo.id);
+    });
+   
+    return done;
+  });
+  
+  Q.when(done, function(done) {
+    console.log("Status: "+done);
+  }, function() {
+    if (arguments[0] instanceof Error) {
+      var e = arguments[0];
+      e._stack = e.stack && e.stack.split('\n');
+      console.error("Purple ERROR: "+e, e);
+    } else {
+      console.error("Purple FAILED", arguments);
+    }
+  });  
+}
+
+function hookWebNav() {
+  var webNavEvents = ['onBeforeNavigate', 'onCommitted', 'onCompleted', 'onCreatedNavigationTarget', 'onDOMContentLoaded', 'onErrorOccurred', 'onReferenceFragmentUpdated'];
+  webNavEvents.forEach(function hookOne(eventType) {
+    chrome.webNavigation[eventType].addListener(function (details) {
+      var last = window.lastWebNavTime || details.timeStamp;
+      console.log(eventType+" "+details.url+" in "+details.tabId+" at "+details.timeStamp+" delta: "+(details.timeStamp-last));
+      window.lastWebNavTime = last;
+    });
+  });
+}
+
+hookWebNav();
+
+function onWindowUnload(event) {
+  window.removeEventListener('unload', onWindowUnload, false);
+  stopMonitor(debuggerError);
+}
+
+window.addEventListener('load', onOuterWindowLoad, false);
 
 }());
