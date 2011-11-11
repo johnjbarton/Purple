e1708eb09a2f0e015bdb33e3d0978f920500e4aa
johnjbarton <johnjbarton@johnjbarton.com>
2011-11-11 14:40:57 -0800

    Fix up message.url equal blank case by using the stack frame

--- log/ConsoleEntry.js
+++ log/ConsoleEntry.js
@@ -7,8 +7,16 @@ function (domplate,                     ConsoleEntryRep) {
   function ConsoleEntry(message) {
     //  http://code.google.com/chrome/devtools/docs/protocol/0.1/console.html#type-ConsoleMessage
     this.message = message;
-    if (message.stackTrace) {
-      this.message.stack = message.stackTrace;
+    var stack = message.stackTrace;
+    if (stack && stack.length) {
+      this.message.stack = stack;
+      // inline scripts sometimes have a blank URL, use the stack for these.
+      if (!this.message.url) {
+        this.message.url = stack[0].url;
+      }
+      if (!this.message.line) {
+        this.message.line = stack[0].lineNumber;
+      }
     }
     this.rep = ConsoleEntryRep;
   }
